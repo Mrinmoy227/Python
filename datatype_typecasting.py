fb7f78411f97bb24db4224b5adc703cbc9f60ec5
Mrinmoy227 <susmisoma85@gmail.com>
2026-03-23 21:55:30 +0600

    DataTypes

---
+++ datatype_typecasting.py
@@ -0,0 +1,21 @@
+name="Penguin"
+age=15
+is_student=True
+weight=35.8
+print("Name:",name)
+print("Data type of name is:",type(name))
+print("Age:",age)
+print("Data types of age is:",type(age))
+print("Is Student?",is_student)
+print("Data types of is_student is:",type(is_student))
+print("Weight:",weight)
+print("Data types of weight is:",type(weight))
+
+print("Type casting:")
+age=str(age)
+print("Data types of age is:",type(age))
+
+num1=2
+num2=9
+result=num1+num2
+print("The addition of two numbers is:",result)
