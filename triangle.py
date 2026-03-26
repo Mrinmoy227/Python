fef829e528b4f7dc35321ee0be6f139e208b6428
Mrinmoy227 <susmisoma85@gmail.com>
2026-03-26 22:12:41 +0600

    Turtle

---
+++ triangle.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to turtle window")
+
+mypen=turtle.Turtle()
+#TRIANGLE
+mypen.forward(100)
+mypen.left(120)
+mypen.forward(100)
+mypen.left(120)
+mypen.forward(100)
+turtle.done()
