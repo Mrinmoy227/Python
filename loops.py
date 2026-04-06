40c2b58551f267f4b5dd2439365a15ff2a2a41e9
Mrinmoy227 <susmisoma85@gmail.com>
2026-04-06 21:51:53 +0600

    List

---
+++ loops.py
@@ -0,0 +1,10 @@
+print('Multiplication table of 17')
+for i in range (1,11):
+ print('17 x',i,"=",17*i)
+print('Sum of numbers 1-10')
+num=1
+sum=0
+while(num<=10):
+  sum=sum+num
+  num=num+1
+print('Answer',sum)
